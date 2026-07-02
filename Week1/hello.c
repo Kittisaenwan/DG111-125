0042f291d4ef67cbfa31cffceb2830c2a7aba830
kittiwin saenwan <kittisaenwan777@gmail.com>
2026-07-02 15:56:32 +0700

    Add flowcharts for grading system and number printing logic
    - Created a flowchart for determining grades based on score input.
    - Added a flowchart for comparing two numbers and displaying the smaller one.
    - Implemented a flowchart for printing numbers from 1 to N based on user input.

---
+++ Week1/hello.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main()
+{
+
+    printf("สวัสดีครับ! ผม Kittiwin saenwan\n");
+    printf("รหัสนักศึกษา: 692110125\n");
+    printf("วิชา: DG111 Computer Programming\n");
+    return 0;
+}
